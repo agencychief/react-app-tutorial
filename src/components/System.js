f65e1e3421da2a580433b1f4b593416b526e4aa2
Daniel Gading <daniel.gading@agencychief.com>
2018-09-27 09:49:02 -0400

    Adds styled components to system

--- src/components/System.js
+++ src/components/System.js
@@ -1,6 +1,35 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import SubSystem from './SubSystem';
+
+const SystemContainer = styled.section`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 15px;
+  margin-bottom: 10px;
+`;
+
+const SystemTitle = styled.h1`
+  width: 100%;
+`;
+
+const SystemInformation = styled.div`
+  flex: 1;
+`;
+
+const SubSystemContainer = styled.div`
+  flex: 2;
+`;
+
+const SubSystemList = styled.ol`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  padding: 0;
+`;
 
 class System extends Component {
 
@@ -34,29 +63,33 @@ class System extends Component {
 
   render() {
     return(
-      <section>
-        <h1>{this.state.name}</h1>
-        <h2>System Stats</h2>
-        <ul>
-          <li>Power Usage: {this.state.totalPowerUsed}</li>
-          <li>Total Power: {this.state.totalPowerUnits}</li>
-          <li>Total SubSystems: {this.state.subSystems.length}</li>
-        </ul>
-        <h2>Sub Systems</h2>
-        <ol>
-          {this.state.subSystems.map((subSystem, index) => {
-            return(
-              <SubSystem
-                key={index}
-                systemName={subSystem.systemName}
-                powerUsage={subSystem.powerUsage}
-                powerUsed={subSystem.powerUsed}
-                updatePowerUsage={(event) => this.updatePowerUsage(event, subSystem.id)}
-              />
-            );
-          })}
-        </ol>
-      </section>
+      <SystemContainer>
+        <SystemTitle>{this.state.name}</SystemTitle>
+        <SystemInformation>
+          <h2>System Stats</h2>
+          <ul>
+            <li>Power Usage: {this.state.totalPowerUsed}</li>
+            <li>Total Power: {this.state.totalPowerUnits}</li>
+            <li>Total SubSystems: {this.state.subSystems.length}</li>
+          </ul>
+        </SystemInformation>
+        <SubSystemContainer>
+          <h2>Sub Systems</h2>
+          <SubSystemList>
+            {this.state.subSystems.map((subSystem, index) => {
+              return(
+                <SubSystem
+                  key={index}
+                  systemName={subSystem.systemName}
+                  powerUsage={subSystem.powerUsage}
+                  powerUsed={subSystem.powerUsed}
+                  updatePowerUsage={(event) => this.updatePowerUsage(event, subSystem.id)}
+                />
+              );
+            })}
+          </SubSystemList>
+        </SubSystemContainer>
+      </SystemContainer>
     );
   }
 }
